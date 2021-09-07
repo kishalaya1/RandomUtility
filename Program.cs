087eaf368d8d5680d3593380179c40a972622ee9
kishalaya1 <30320323+kishalaya1@users.noreply.github.com>
2021-09-07 15:41:00 +0530

    initial commit
    initial commit for my repository

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static RandomUtility.Num;
+
+namespace RandomUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Please enter No : ");
+            int inputNo = Convert.ToInt32(Console.ReadLine());
+            int result = SumOfDigits(inputNo);
+            Console.WriteLine("The result is : " + result);
+        }
+    }
+}
